3f9d73e3ca6a04607b892438adec2e778ba84289
DevInCube <ruslan.hadyniak@gmail.com>
2015-03-10 19:38:10 +0200

    wiki module and html parse

---
+++ My.OntoTools/NLP/html_preproc.py
@@ -0,0 +1,21 @@
+#@todo create link parser and filter for html docs general and for wikipedia in one case
+
+from bs4 import BeautifulSoup
+import re
+from urllib.request import urlopen
+
+html_page = urlopen("http://en.wikipedia.org/wiki/Solar_System")
+soup = BeautifulSoup(html_page)
+bodyName = 'mw-content-text'
+soup = soup.find("div", {"id": bodyName})
+i = 0
+for link in soup.findAll('a'):
+    i += 1
+    title = link.get('title')
+    ref = link.get('href')
+    if not title : title = '<none>'
+    if not ref : ref = '<none>'
+    try:
+        print(i, ') ', title, ' | ', ref)
+    except:
+        print('<error>')
